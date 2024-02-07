348e3824d6cbc4c7effb584cc4918be1bd6146ed
URAL KAMALETDINOV <urykama@gmail.com>
2024-02-07 20:46:44 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import newFile
+
+
+def print_hi():
+    newFile.def01()
+    newFile.def02()
+
+
+if __name__ == '__main__':
+    print_hi()
---
+++ newFile.py
@@ -0,0 +1,8 @@
+def def01():
+    print("Hello, world!")
+    print("Это функция def01")
+
+
+def def02():
+    print("Hello, world!")
+    print("Это функция def02")
